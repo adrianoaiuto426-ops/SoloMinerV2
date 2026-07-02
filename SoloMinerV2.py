16ab2c849385ba182f207d33b0be5591a291c991
adrianoaiuto426-ops <adrianoaiuto426@gmail.com>
2026-07-01 20:11:50 -0400

    Update SoloMinerV2.py
    Signed-off-by: adrianoaiuto426-ops <adrianoaiuto426@gmail.com>

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = '0xBa4FA9eeB6154bD0ecFfc42FD5696037Fd9912D4' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
